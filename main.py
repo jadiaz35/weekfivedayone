651b4b7e8729ffd19ce7d47d7dc1eaf3e839a08e
Juan Diaz <113930348+jadiaz35@users.noreply.github.com>
2022-09-21 17:37:14 +0000

    dictionary ting

--- main.py
+++ main.py
@@ -134,12 +134,22 @@ print(math)
 
 # dictionaries
 # another data structure that stores data that can be unstructured
-# employee = {'first_name': 'steven',
-#             'last_name': 'pretti',
-#              'weight': 37.2,
-#               'size': 5.77
-#              'height:' 6}
-# print(employee['weight'])
+employee = {'first_name': 'steven',
+            'last_name': 'pretti',
+             'weight': 37.2,
+              'size': 5.77,
+             'height': 6}
+print(employee['height'])
+
+warrior = { 'made_by' : 'hbomax',
+          'release_date': 2018,
+          'budget' : '24million',
+          'rotten_tomatoes_score': 'fresh'}
+warrior['score'] = 78
+print(warrior['budget'])
+print(warrior.keys())
+print(warrior.items())
+
 
 #challenge 1
 # Create a dictionary called my_dict that stores the following information about a person:
@@ -149,6 +159,12 @@ print(math)
 # occupation: Journalist
 # The names of the keys and values must be equal to the ones indicated above.
 
+my_dict = {'name' : 'Karen',
+          'surname' : 'Jurgens',
+          'age': 35,
+          'occupation': 'journalist'}
+print(my_dict.items())
+print(my_dict.keys())
 #challenge 2:
 # Use print to returns the second item of the list called points2, inside the following dictionary.
 
